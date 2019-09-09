f962dc307f808e304708ca5e36cfc36498cdbcac
HashimaHz <54021321+HashimaHz@users.noreply.github.com>
2019-09-09 22:42:49 +0700

    Update liff.js

--- liff.js
+++ liff.js
@@ -256,8 +256,9 @@ function liffGetButtonStateCharacteristic(characteristic) {
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
             const val = (new Uint8Array(e.target.value.buffer))[0];
+            const val1 = (new Uint8Array(e.target.value.buffer))[1];
             showTemp(val);
-            showMoisture(val);
+            showMoisture(val1);
             if (val > 0) {
                 // press
                 uiToggleStateButton(true);
