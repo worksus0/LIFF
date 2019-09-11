af9c17f216710989a17ace7d3dbaf2c402e04c76
HashimaHz <54021321+HashimaHz@users.noreply.github.com>
2019-09-11 20:26:03 +0700

    Add files via upload

---
+++ LT_test_1_32.ino
@@ -0,0 +1,212 @@
+#include <ArduinoJson.h>
+
+#include <BLEServer.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <SimpleDHT.h>
+
+
+
+// Device Name: Maximum 30 bytes
+#define DEVICE_NAME "LINE Things 1"
+
+// User service UUID: Change this to your generated service UUID
+// Change USER_SERVICE_UUID date from https://developers.line.biz/en/reference/line-things/#post-trial-products
+#define USER_SERVICE_UUID "ba2f7ca1-7c97-4019-a59a-c9ed6acfc4b8"
+// User service characteristics
+#define WRITE_CHARACTERISTIC_UUID "E9062E71-9E62-4BC6-B0D3-35CDCD9B027B"
+#define NOTIFY_CHARACTERISTIC_UUID "62FBD229-6EDD-4D1A-B554-5C4E1BB29169"
+
+// PSDI Service UUID: Fixed value for Developer Trial
+#define PSDI_SERVICE_UUID "E625601E-9E55-4597-A598-76018A0D293D"
+#define PSDI_CHARACTERISTIC_UUID "26E2B12B-85F0-4F3F-9FDD-91D114270E6E"
+
+#define BUTTON 0
+#define LED1 2
+
+// DHT22
+int pinDHT22 = 13;
+SimpleDHT22 dht22(pinDHT22);
+float temperature = 0;
+float humidity = 0;
+float moisture = 0;
+
+BLEServer* thingsServer;
+BLESecurity *thingsSecurity;
+BLEService* userService;
+BLEService* psdiService;
+BLECharacteristic* psdiCharacteristic;
+BLECharacteristic* writeCharacteristic;
+BLECharacteristic* notifyCharacteristic;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+volatile int btnAction = 0;
+
+class serverCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+   deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+class writeCallback: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *bleWriteCharacteristic) {
+    std::string value = bleWriteCharacteristic->getValue();
+    if ((char)value[0] <= 1) {
+      digitalWrite(LED1, (char)value[0]);
+    }
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, 0);
+  pinMode(BUTTON, INPUT_PULLUP);
+  attachInterrupt(BUTTON, buttonAction, CHANGE);
+
+  BLEDevice::init("");
+  BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
+
+  // Security Settings
+  BLESecurity *thingsSecurity = new BLESecurity();
+  thingsSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_ONLY);
+  thingsSecurity->setCapability(ESP_IO_CAP_NONE);
+  thingsSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+
+  setupServices();
+  startAdvertising();
+  Serial.println("Ready to Connect");
+}
+
+void loop() {
+  uint8_t btnValue;
+//  delay(2000);
+//  getTemp();
+//  sendTemp();
+//  getMoisture();
+//  sendMoisture();
+
+  getData();
+  sendData();
+
+  while (btnAction > 0 && deviceConnected) {
+    btnValue = !digitalRead(BUTTON);
+    btnAction = 0;
+    notifyCharacteristic->setValue(&btnValue, 1);
+    notifyCharacteristic->notify();
+    delay(20);
+  }
+  // Disconnection
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // Wait for BLE Stack to be ready
+    thingsServer->startAdvertising(); // Restart advertising
+    oldDeviceConnected = deviceConnected;
+  }
+  // Connection
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+  }
+}
+
+void setupServices(void) {
+  // Create BLE Server
+  thingsServer = BLEDevice::createServer();
+  thingsServer->setCallbacks(new serverCallbacks());
+
+  // Setup User Service
+  userService = thingsServer->createService(USER_SERVICE_UUID);
+  // Create Characteristics for User Service
+  writeCharacteristic = userService->createCharacteristic(WRITE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+  writeCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  writeCharacteristic->setCallbacks(new writeCallback());
+
+  notifyCharacteristic = userService->createCharacteristic(NOTIFY_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+  notifyCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  BLE2902* ble9202 = new BLE2902();
+  ble9202->setNotifications(true);
+  ble9202->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  notifyCharacteristic->addDescriptor(ble9202);
+
+  // Setup PSDI Service
+  psdiService = thingsServer->createService(PSDI_SERVICE_UUID);
+  psdiCharacteristic = psdiService->createCharacteristic(PSDI_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+  psdiCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+  // Set PSDI (Product Specific Device ID) value
+  uint64_t macAddress = ESP.getEfuseMac();
+  psdiCharacteristic->setValue((uint8_t*) &macAddress, sizeof(macAddress));
+
+  // Start BLE Services
+  userService->start();
+  psdiService->start();
+}
+
+void startAdvertising(void) {
+  // Start Advertising
+  BLEAdvertisementData scanResponseData = BLEAdvertisementData();
+  scanResponseData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
+  scanResponseData.setName(DEVICE_NAME);
+
+  thingsServer->getAdvertising()->addServiceUUID(userService->getUUID());
+  thingsServer->getAdvertising()->setScanResponseData(scanResponseData);
+  thingsServer->getAdvertising()->start();
+}
+
+void buttonAction() {
+  btnAction++;
+}
+
+void getData() {
+  int sensor_analog;
+  sensor_analog = analogRead(A0);
+  moisture = ( 100 - ( (sensor_analog/4095.00) * 100 ) );
+  Serial.print("Moisture Percentage = ");
+  Serial.print(moisture);
+  Serial.print("%\n\n");
+
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT22 failed, err="); Serial.println(err);delay(2000);
+    return;
+  }
+
+  Serial.print("Sample OK: ");
+  Serial.print((float)temperature); Serial.print(" *C, ");
+  Serial.print((float)humidity); Serial.println(" RH%");
+  
+  // DHT22 sampling rate is 0.5HZ.
+  delay(2500);
+}
+
+void sendData() {
+
+  StaticJsonBuffer<200> jsonBuffer;
+  
+  // Build your own object tree in memory to store the data you want to send in the request
+  JsonObject& root = jsonBuffer.createObject();
+  root["sensor"] = "LINE Things";
+  
+  JsonObject& data = root.createNestedObject("data");
+  data.set("t", temperature);
+  data.set("h", humidity);
+  data.set("m", moisture);
+ 
+  String captureData;
+  root.printTo(captureData);
+  
+  uint8_t broadcastData = moisture;
+  notifyCharacteristic->setValue(&broadcastData, 1);
+  notifyCharacteristic->notify();
+  
+  Serial.println("Send to line");
+  
+  delay(1000);
+} 
